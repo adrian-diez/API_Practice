5664d4f246f31e7d27d43578e3cc6dcc4316f2a9
= <adrian.diezg@hotmail.com>
2022-01-27 22:08:12 +0100

    login done

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
-
+const jwt = require('jsonwebtoken')
 const UserModel = require('../models/user.model') 
 
 // Signup route 
@@ -11,6 +11,27 @@ router.post('/signup', async (req, res) =>{
     res.json(user)
   } catch (error) {
     console.error(error) 
+  }
+})
+
+router.post('/auth/login', async (req, res) => {
+  try {
+
+    const user = await UserModel.find({email : req.body.email})
+
+    if (!user) return res.json({error : 'user/password not found'}) //If user doesn't exist, send error
+
+
+    if(bcrypt.compareSync(req.body.password, user.password)){
+
+      const data = {user_id : user._id}
+      jwt.sign(data, process.env.SECRET, {expiresIn : 60*60})
+
+      res.json({token})
+    }
+
+  } catch (error) {
+    console.error(error)
   }
 })
 
